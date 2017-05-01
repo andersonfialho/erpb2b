d38676ee9aab5186cf907b6b37f244c28fc25c2c
Anderson <Anderson@B2BTI>
2017-04-30 21:13:25 -0300

    TESTE

--- PDV-B2B/PDV-B2B/Form1.Designer.cs
+++ PDV-B2B/PDV-B2B/Form1.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verifica Video";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 64);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Verifica Video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(59, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 64);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Verifica Video";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 287);
+            this.ClientSize = new System.Drawing.Size(473, 308);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- PDV-B2B/PDV-B2B/Form1.cs
+++ PDV-B2B/PDV-B2B/Form1.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//https://www.youtube.com/watch?v=jzroyypTGoU
+//https://pt.stackoverflow.com/questions/46474/como-fazer-um-bot%C3%A3o-com-cantos-arredondados-em-c-winforms?rq=1
+//https://www.youtube.com/watch?v=UMhskLXJuq4
+
+
 namespace PDV_B2B
 {
     public partial class Form1 : Form
@@ -15,6 +20,29 @@ namespace PDV_B2B
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //WindowState = FormWindowState.Maximized;
+                        
+            //this.Top = 0;
+            //this.Left = 0;
+            // this.Width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            // this.Height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Width = 800;//Altera a Largura
+            this.Height = 600;//Altera a Altura
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Width = 1024;//Altera a Largura
+            this.Height = 768;//Altera a Altura
         }
     }
 }
